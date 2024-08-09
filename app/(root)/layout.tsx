c987d1f2dae26c863098396131dad1f5130db185
Temitope segun Sule <tope@agencyleroy.com>
2024-08-10 01:06:05 +0300

    implemented the side links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,26 @@
 import Sidebar from "@/components/ui/SideBar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const loggedIn = { firstname: 'Adrian', lastname: 'JSM' };
+
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
-      {children}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,26 +2,40 @@ import React from 'react'
 import "./globals.css";
 import HeaderBox from '@/components/ui/HeaderBox';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import RightSidebar from '@/components/ui/RightSidebar';
 const Home = () => {
-  const loggedIn = {firstname: 'Adrian'}
+  const loggedIn = {
+    firstName: 'Adrian', lastName: 'John',
+    email: 'contact@jsmasterypro'
+
+  }
   return (
-    <section className="home">
+    <section className="home" >
       <div className="home-content">
         <header className="home-header">
           <HeaderBox type="greeting"
             title="welcome"
-            user={loggedIn?.firstname || 'guest'}
+            user={loggedIn?.firstName || 'guest'}
             subtext="Aceess and manage your account effeciantly." />
-          
+
           <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
-          
+
         </header>
+
       </div>
-    </section>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
+
+
+    </section >
+
   )
 }
 
